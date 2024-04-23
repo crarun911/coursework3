b82f75f422a87b2294f01989ed2531ecf9c6a029
crarun911 <crarun911@gmail.com>
2024-04-23 12:47:38 +0100

    Registration form added and route added for Registration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('register', [UserController::class, 'showRegistrationForm'])->name('register');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
